bf320475e668f6e5a79fd39b353376acc81d7f55
jpass23 <jpass23@live.unc.edu>
2023-01-23 23:33:30 -0500

    Initial Commit

---
+++ Openings/OpeningsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpeningsApp.swift
+//  Openings
+//
+//  Created by Jaden Passero on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct OpeningsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
