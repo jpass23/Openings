126327b1d379c861e704a61e8bdb121243ecf614
jpass23 <jpass23@live.unc.edu>
2023-01-23 23:37:51 -0500

    Added extensions

--- Openings/ContentView.swift
+++ Openings/ContentView.swift
@@ -24,3 +24,9 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+// MARK: Extensions
+extension UIScreen {
+    static let screenWidth = UIScreen.main.bounds.size.width
+    static let screenHeight = UIScreen.main.bounds.size.height
+}
